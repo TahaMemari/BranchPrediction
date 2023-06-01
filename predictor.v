4b4909b849a0dee4b0f29d0219d3b333d27c7ab6
TahaMemari <92461350+TahaMemari@users.noreply.github.com>
2023-06-01 11:07:31 +0330

    Update predictor.v

--- predictor.v
+++ predictor.v
@@ -2,29 +2,28 @@ module predictor(
    input wire request,      // Request to make prediction   
    input wire clk,          // Clock signal
    input wire taken,        // Feedback if previous branch was taken  
+   input wire result,
    output reg prediction    // Output prediction
 );
 
-   reg [1:0] counter = 2'd0;  // 2-bit Up/Down counter
+   reg [1:0] counter = 2'b11;  // 2-bit Up/Down counter
       
    always @(posedge clk) begin
-      if (request) begin    
+      if (result) begin    
          if (taken) begin
             if (counter < 2'd3)    
                counter <= counter + 1'b1; 
-            else
-               counter <= 2'd3; 
          end
          else begin   
             if (counter > 2'd0)
-               counter <= counter - 1'b1;
-            else      
-               counter <= 2'd0;                
+               counter <= counter - 1'b1;             
          end 
          
          // Prediction based on MSB of counter           
-         prediction <= counter[1];           
-      end           
+                    
+      end   
+      if (request)
+         prediction <= counter[1];
    end
                
 endmodule
